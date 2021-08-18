1cbb3f0cee58d6e6078329774a1d8964982a3b59
Thiago Bignotto <bignotto@gmail.com>
2021-08-18 06:38:45 -0300

    save commit

--- src/app.ts
+++ src/app.ts
@@ -1,19 +1,31 @@
-import express from 'express';
+import express from "express";
+import swaggerUi from "swagger-ui-express";
 
-import { categoryRoutes } from './routes/categories.routes';
-import { specificationRoutes } from './routes/specifications.routes';
+import swaggerFile from "../swagger.json";
+import { categoryRoutes } from "./routes/categories.routes";
+import { specificationRoutes } from "./routes/specifications.routes";
 
 const app = express();
+console.log(swaggerFile);
 
 app.use(express.json());
+app.use(
+  "/api-docs",
+  swaggerUi.serve,
+  swaggerUi.setup({
+    swaggerFile,
+  })
+);
 app.use(categoryRoutes);
 app.use(specificationRoutes);
 
-app.use('/', (request, response) => response.status(200).json({
-  application: 'Rental X API',
-  description: 'Car Rental API',
-  author: 'Thiago Bignotto',
-  contact: 'bignotto@gmail.com',
-}));
+app.use("/", (request, response) =>
+  response.status(200).json({
+    application: "Rental X API",
+    description: "Car Rental API",
+    author: "Thiago Bignotto",
+    contact: "bignotto@gmail.com",
+  })
+);
 
 export { app };
