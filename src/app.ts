c2c420f55d97c9d04f01a8c42767532b660eadbf
Thiago Bignotto <bignotto@gmail.com>
2021-08-18 06:44:31 -0300

    save commit swagger fixed

--- src/app.ts
+++ src/app.ts
@@ -1,21 +1,14 @@
 import express from "express";
 import swaggerUi from "swagger-ui-express";
 
-import swaggerFile from "../swagger.json";
 import { categoryRoutes } from "./routes/categories.routes";
 import { specificationRoutes } from "./routes/specifications.routes";
+import swaggerFile from "./swagger.json";
 
 const app = express();
-console.log(swaggerFile);
 
 app.use(express.json());
-app.use(
-  "/api-docs",
-  swaggerUi.serve,
-  swaggerUi.setup({
-    swaggerFile,
-  })
-);
+app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerFile));
 app.use(categoryRoutes);
 app.use(specificationRoutes);
 
