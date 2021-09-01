649d453b8c384eea9a5dc169bff840f7d3e2faab
Thiago Bignotto <bignotto@gmail.com>
2021-09-01 07:35:12 -0300

    save commit

---
+++ src/modules/accounts/useCases/authenticateUser/AuthenticateUserController.ts
@@ -0,0 +1,25 @@
+import { Request, Response } from "express";
+import { container } from "tsyringe";
+
+import { AuthenticateUserUseCase } from "./AuthenticateUserUseCase";
+
+class AuthenticateUserController {
+  async handle(request: Request, response: Response): Promise<Response> {
+    const { email, password } = request.body;
+
+    const authenticateUserUseCase = container.resolve(AuthenticateUserUseCase);
+
+    try {
+      const token = await authenticateUserUseCase.execute({ email, password });
+      return response.status(200).json(token);
+    } catch (error) {
+      return response.status(500).json({
+        where: "Authenticate User",
+        message: "use case execute error",
+        error,
+      });
+    }
+  }
+}
+
+export { AuthenticateUserController };
