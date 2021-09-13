c4db807c14db959ebc8f7f607e21516b6039d3ec
Thiago Bignotto <bignotto@gmail.com>
2021-09-13 07:05:51 -0300

    save commit

---
+++ src/modules/cars/infra/typeorm/entities/Car.ts
@@ -0,0 +1,4 @@
+import { Entity } from "typeorm";
+
+@Entity("cars")
+class Car {}
---
+++ src/modules/cars/useCases/createCar/CreateCarUseCase.spec.ts
@@ -0,0 +1,8 @@
+import { inject, injectable } from "tsyringe";
+
+@injectable()
+class CreateCarUseCase {
+  async execute(): Promise;
+}
+
+export { CreateCarUseCase };
