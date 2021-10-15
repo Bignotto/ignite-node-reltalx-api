9233131d89e547c8b0065bd0d374c1a5fe9dca4a
Thiago Bignotto <bignotto@gmail.com>
2021-10-15 06:57:11 -0300

    implemented S3 storage provider

--- src/shared/container/providers/index.ts
+++ src/shared/container/providers/index.ts
@@ -2,7 +2,11 @@ import { container } from "tsyringe";
 
 import { DayjsDateProvider } from "./DateProvider/implementations/DayjsDateProvider";
 import { EtherealMailProvider } from "./MailProvider/implementations/EtherealMailProvider";
+import { LocalStorageProvider } from "./StorageProvider/implementations/LocalStorageProvider";
+import { S3StorageProvider } from "./StorageProvider/implementations/S3StorageProvider";
 
 container.registerSingleton("DateProvider", DayjsDateProvider);
 
 container.registerInstance("MailProvider", new EtherealMailProvider());
+
+container.registerSingleton("StorageProvider", S3StorageProvider);
