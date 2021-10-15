f5d01b4e76d0ceb4f246da63464e7d563cd137b3
Thiago Bignotto <bignotto@gmail.com>
2021-10-15 07:03:03 -0300

    implemented storage provider selection in env file

--- src/shared/container/providers/index.ts
+++ src/shared/container/providers/index.ts
@@ -5,8 +5,16 @@ import { EtherealMailProvider } from "./MailProvider/implementations/EtherealMai
 import { LocalStorageProvider } from "./StorageProvider/implementations/LocalStorageProvider";
 import { S3StorageProvider } from "./StorageProvider/implementations/S3StorageProvider";
 
+const diskStorage = {
+  local: LocalStorageProvider,
+  s3: S3StorageProvider,
+};
+
 container.registerSingleton("DateProvider", DayjsDateProvider);
 
 container.registerInstance("MailProvider", new EtherealMailProvider());
 
-container.registerSingleton("StorageProvider", S3StorageProvider);
+container.registerSingleton(
+  "StorageProvider",
+  diskStorage[process.env.STORAGE_PROVIDER]
+);
